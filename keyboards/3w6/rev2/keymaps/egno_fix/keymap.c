c352adb7835b898adffe0a079022011ec72396e2
Alex Shel <ex.lxndr@gmail.com>
2024-02-05 20:24:38 +0200

    slsh

--- keyboards/3w6/rev2/keymaps/egno_fix/keymap.c
+++ keyboards/3w6/rev2/keymaps/egno_fix/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_GAME] = LAYOUT(
         KC_TAB,   KC_Q,         KC_W,    KC_E,    KC_R,                                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_LALT,   KC_A,         KC_S,    KC_D,    KC_F,                                                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_LSFT,   KC_Z,         KC_X,    KC_C,    KC_V,                                                    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
+        KC_LSFT,   KC_Z,         KC_X,    KC_C,    KC_V,                                                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                                         TD(TD_R_BSPC_CFG_TAB), KC_SPC, KC_ENT,                           LT(_SYM, KC_BSPC), KC_ENT, LALT_T(KC_DEL)
     ),
     [_SYM] = LAYOUT(
