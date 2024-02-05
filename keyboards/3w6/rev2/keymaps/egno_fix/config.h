de927c92f5f9e98d19e79506e37214068101b496
Alex Shel <ex.lxndr@gmail.com>
2024-02-05 19:49:53 +0200

    egno fix

---
+++ keyboards/3w6/rev2/keymaps/egno_fix/config.h
@@ -0,0 +1,9 @@
+#pragma once
+#define TAPPING_TERM 225
+#define COMBO_TERM 30
+#define AUTO_SHIFT_MODIFIERS
+#define AUTO_SHIFT_TIMEOUT 175
+#define NO_AUTO_SHIFT_SPECIAL
+#define ONESHOT_TAP_TOGGLE 50  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define I2C_TIMEOUT 10
